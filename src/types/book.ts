7fb0ec9258943eaf3fabf6c0fcb9367c9424b8a6
lazors <s.lazorenko10@gmail.com>
2025-10-25 23:42:20 +0200

    feat: add UI components for skeleton, slider, sonner, switch, table, tabs, textarea, toggle group, toggle, tooltip

---
+++ src/types/book.ts
@@ -0,0 +1,17 @@
+export type DeliveryStatus = 'No' | 'Shipped' | 'Yes';
+export type ForSaleStatus = 'No' | 'Maybe' | 'Yes';
+
+export interface Book {
+  id: string;
+  title: string;
+  publisher: string;
+  preOrderStartDate: string;
+  estimatedDeliveryDate: string;
+  deliveryAddress: string;
+  ordered: boolean;
+  delivered: DeliveryStatus;
+  totalPrice: number;
+  quantity: number;
+  forSale: ForSaleStatus;
+  soldFor: number | null;
+}
